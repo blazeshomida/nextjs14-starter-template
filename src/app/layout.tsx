d33cf07e0d77aa2ca4a9b2c1b451badfe90c5bdb
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-05-20 12:48:14 -1000

    feat(query): :sparkles: add tanstack-query/react support and devtools
    - :heavy_plus_sign: Added `@tanstack/react-query` and `@tanstack/react-query-devtools` dependencies to `package.json`
    - :heavy_plus_sign: Updated `pnpm-lock.yaml` with `@tanstack/react-query` and `@tanstack/react-query-devtools`
    - :sparkles: Created `QueryProvider` component in `src/app/_providers/QueryProvider.tsx`
    - :sparkles: Integrated `QueryProvider` and React Query Devtools in `src/app/layout.tsx` to enable React Query
    - :bug: Added `suppressHydrationWarning` attribute to `html` element for next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "./_providers/QueryProvider";
 import { ThemeProvider } from "./_providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,16 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
